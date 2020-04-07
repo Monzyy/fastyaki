c7f87c8078ad7a61b137205c1560d567dccd14d8
Magnus Hansen <Magnushansen12@hotmail.com>
2020-04-07 21:09:32 +0200

    Add script for moving fast5 files to one directory

---
+++ bin/movefast5.py
@@ -0,0 +1,19 @@
+import os
+import re
+import argparse
+
+from shutil import copyfile
+
+parser = argparse.ArgumentParser(description='Split a dataset.')
+parser.add_argument('source', help='source directory')
+parser.add_argument('dest', help='destination directory')
+args = parser.parse_args()
+
+for subdir, dirs, files in os.walk(args.source):
+    for file in files:
+        if re.search('.txt', file):
+            continue
+        new_filename = subdir[subdir.rfind('/') + 1:] + '.fast5'
+        copyfile(os.path.join(subdir, file), os.path.join(subdir, new_filename))
+        os.rename(os.path.join(subdir, new_filename), os.path.join(args.dest, new_filename))
+
