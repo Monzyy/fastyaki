1ef2e0273214808bf8f5aeac18e33d74636e8e5c
Monzyy <Magnushansen12@hotmail.com>
2020-04-08 12:34:28 +0200

    Let users specify path to convert-data script

--- convert_bonito_data_bac8.sh
+++ convert_bonito_data_bac8.sh
@@ -1,8 +1,12 @@
 SOURCE=$1
-NAME_TEMPLATE=${$2:bac8_0.05_test}
+CONVERT_DATA_SCRIPT=$2
+NAME_TEMPLATE=${3:-bac8_0.05_test}
+
 
 for CUR_BAC in Bacillus Enterococcus Escherichia Lactobacillus Listeria Pseudomonas Salmonella Staphylococcus
 do
   CUR_DIR=$SOURCE/$NAME_TEMPLATE${CUR_BAC}
-  ./scripts/convert-data $CUR_DIR/train.hdf5 $CUR_DIR --chunks 24000 --min-run 2
+  echo "Converting ${CUR_BAC}"
+  python3 $CONVERT_DATA_SCRIPT $CUR_DIR/train.hdf5 $CUR_DIR --chunks 24000 --min-run 2
 done
+echo "Done"
